6fdeb3af0ccef06c0e443bcc22dae38f7cf2f13d
zaal6006 <alex.zaprometov@gmail.com>
2021-04-08 13:58:00 -0400

    First commit

---
+++ src/main/java/ru/alex/springcourse/controllers/PeopleController.java
@@ -0,0 +1,35 @@
+package ru.alex.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.alex.springcourse.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        // Fetch all peoples objects by DAO and pass it to the view(s)
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // get one account by id using DAO then pass it to the view
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
---
+++ src/main/java/ru/alex/springcourse/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package ru.alex.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alex.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person( ++PEOPLE_COUNT, "Dustin"));
+        people.add(new Person(++PEOPLE_COUNT, "Will"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Lucas"));
+        people.add(new Person(++PEOPLE_COUNT, "Eleven"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
