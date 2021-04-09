546dadea128f2baf83193f5165ac7e94cae70b3c
zaal6006 <alex.zaprometov@gmail.com>
2021-04-09 18:47:05 -0400

    Lessong: 27 - completed!

--- src/main/java/ru/alex/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alex/springcourse/dao/PersonDAO.java
@@ -1,17 +1,85 @@
 package ru.alex.springcourse.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.alex.springcourse.models.Person;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    public static JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index()  {
+//        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person show(int id) {
+//        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new PersonMapper())
+//                .stream().findAny().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},
+                                    new BeanPropertyRowMapper<>(Person.class))
+                                    .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)",
+                person.getName(), person.getAge(), person.getEmail());
+    }
+
+    public void update(int id, Person updatedPerson) {
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?",
+                updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+}
+
+
+/*
+
+
+-- create table Person(
+--     id int,
+--     name varchar,
+--     age int,
+--     email varchar
+-- );
+--
+-- insert into Person values (1,'Dustin Henderson', 12, 'dustin.henderson@hawkins.com');
+-- insert into Person values (2,'Lucas Sinclair', 12, 'lucas.sinclair@hawkins.com');
+-- insert into Person values (3,'Mike Wheeler', 12, 'mike.wheeler@hawkins.com');
+-- insert into Person values (4,'Will Byers', 12, 'will.byers@hawkins.com');
+
+select * from person;
+
+//    private List<Person> people;
+
+//    {
+//        people = new ArrayList<>();
+//
+//        people.add(new Person(++PEOPLE_COUNT, "Dustin Henderson", 12, "dustin@hawkins.com"));
+//        people.add(new Person(++PEOPLE_COUNT, "Lucas Sinclair", 12, "lucas@hawkins.com"));
+//        people.add(new Person(++PEOPLE_COUNT, "Mike Wheeler", 12, "mike@hawkins.comm"));
+//        people.add(new Person(++PEOPLE_COUNT, "Will Byers", 12, "will@hawkins.com"));
+//    }
+ */
+
+/*
+-- code for creating and connection to db:
+
+private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "postgres";
 
@@ -30,6 +98,9 @@ public class PersonDAO {
             throwables.printStackTrace();
         }
     }
+ */
+
+/*
 
     public List<Person> index()  {
         List<Person> people = new ArrayList<>();
@@ -55,8 +126,13 @@ public class PersonDAO {
 
         return people;
     }
+ */
 
-    public Person show(int id) {
+/*
+
+Methods update/delete:
+
+public Person show(int id) {
 //        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
 
         Person person = null;
@@ -129,34 +205,5 @@ public class PersonDAO {
 
 //        people.removeIf(p -> p.getId() == id);
     }
-}
 
-
-/*
-
-
--- create table Person(
---     id int,
---     name varchar,
---     age int,
---     email varchar
--- );
---
--- insert into Person values (1,'Dustin Henderson', 12, 'dustin.henderson@hawkins.com');
--- insert into Person values (2,'Lucas Sinclair', 12, 'lucas.sinclair@hawkins.com');
--- insert into Person values (3,'Mike Wheeler', 12, 'mike.wheeler@hawkins.com');
--- insert into Person values (4,'Will Byers', 12, 'will.byers@hawkins.com');
-
-select * from person;
-
-//    private List<Person> people;
-
-//    {
-//        people = new ArrayList<>();
-//
-//        people.add(new Person(++PEOPLE_COUNT, "Dustin Henderson", 12, "dustin@hawkins.com"));
-//        people.add(new Person(++PEOPLE_COUNT, "Lucas Sinclair", 12, "lucas@hawkins.com"));
-//        people.add(new Person(++PEOPLE_COUNT, "Mike Wheeler", 12, "mike@hawkins.comm"));
-//        people.add(new Person(++PEOPLE_COUNT, "Will Byers", 12, "will@hawkins.com"));
-//    }
  */
---
+++ src/main/java/ru/alex/springcourse/dao/PersonMapper.java
@@ -0,0 +1,23 @@
+package ru.alex.springcourse.dao;
+
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.alex.springcourse.models.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
+    }
+
+}
